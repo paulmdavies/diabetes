3eb69f45f782a165b931073faae6dda6522d4536
Paul Davies <paulmdavies@gmail.com>
2015-02-13 18:16:21 +0000

    Set up diabetes project and add to gitignore

---
+++ project/build.scala
@@ -0,0 +1,17 @@
+import sbt._
+import Keys._
+
+object DiabetesBuild extends Build
+{
+    lazy val root = Project(
+        id = "Diabetes",
+        base = file( "." ),
+        settings = Defaults.defaultSettings ++ Seq(
+            scalaVersion := "2.11.5",
+            libraryDependencies ++= Seq(
+                "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+                "org.scalacheck" %% "scalacheck" % "1.12.1" % "test"
+            )
+        )
+    )
+}
