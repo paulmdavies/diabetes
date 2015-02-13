9088260f163c288efcad2d9631f8a28b72ae54f3
Paul Davies <paulmdavies@gmail.com>
2015-02-13 18:31:45 +0000

    Added simple test JavaFX application

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,8 @@ object DiabetesBuild extends Build
             scalaVersion := "2.11.5",
             libraryDependencies ++= Seq(
                 "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-                "org.scalacheck" %% "scalacheck" % "1.12.1" % "test"
+                "org.scalacheck" %% "scalacheck" % "1.12.1" % "test",
+                "org.scalafx" %% "scalafx" % "8.0.0-R4"
             )
         )
     )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+
+object Main extends JFXApp
+{
+    stage = new PrimaryStage
+    {
+        title = "Diabetes"
+    }
+}
